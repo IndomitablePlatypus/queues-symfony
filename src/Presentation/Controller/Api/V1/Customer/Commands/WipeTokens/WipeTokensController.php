ec6462be6f926fb141476ed7e2a1bc17fa3f4215
IndomitablePlatypus <arutomi@gmail.com>
2022-06-22 12:07:29 +0300

    Add rapidoc style docs

--- src/Presentation/Controller/Api/V1/Customer/Commands/WipeTokens/WipeTokensController.php
+++ src/Presentation/Controller/Api/V1/Customer/Commands/WipeTokens/WipeTokensController.php
@@ -5,6 +5,8 @@ namespace App\Presentation\Controller\Api\V1\Customer\Commands\WipeTokens;
 use App\Config\Routing\RouteName;
 use App\Domain\Messages\ClearTokens;
 use App\Presentation\Controller\Api\V1\ApiController;
+use App\Presentation\Controller\Api\V1\Customer\Commands\Register\Input\RegisterRequest;
+use Nelmio\ApiDocBundle\Annotation\Model;
 use OpenApi\Attributes as OA;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Messenger\MessageBusInterface;
@@ -14,19 +16,23 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/api/v1/customer')]
 class WipeTokensController extends ApiController
 {
+    /**
+     * Clear user tokens
+     *
+     * Removes all existing access tokens for the current user. (I.e. logout)
+     */
     #[OA\Response(
         response: 200,
-        description: "Removes all existing access tokens for the current user. (I.e. logout)",
-        content: new OA\MediaType(
-            mediaType: "json",
-            schema: new OA\Schema(
-                description: "Customer Id",
-                type: "string",
-                format: "uuid",
-                nullable: false,
-            )
+        description: "All tokens successfully cleared",
+        content: new OA\JsonContent(
+            description: "Customer Id",
+            type: "string",
+            example: "41c8613d-6ae2-41ad-841a-ffd06a116961",
+            nullable: false,
         )
     )]
+    #[OA\Response(ref: "#/components/responses/AuthenticationException", response: 401)]
+    #[OA\Response(ref: "#/components/responses/UnexpectedException", response: 500)]
     #[Route('/wipe-tokens', name: RouteName::CLEAR_TOKENS, methods: ['GET'])]
     public function register(MessageBusInterface $messageBus): JsonResponse
     {
